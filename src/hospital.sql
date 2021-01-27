683a39e3613ce1446b0469e4e9437ffe813774f8
Raihan Khan <raihankhanraka@gmail.com>
2021-01-28 01:04:43 +0600

    Update hospital.sql

--- src/hospital.sql
+++ src/hospital.sql
@@ -1,30 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Feb 02, 2016 at 10:56 AM
--- Server version: 10.1.8-MariaDB
--- PHP Version: 5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `hospital`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `medication`
---
 
 CREATE TABLE `medication` (
   `id` int(11) NOT NULL,
@@ -173,6 +149,3 @@ ALTER TABLE `medicine`
 --
 ALTER TABLE `patient`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
